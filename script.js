f2533ef1a559cd4fb499bdc2f36c029d9d16d5bd
Harsha KS <harsha28sathish@gmail.com>
2022-01-25 21:21:14 +0530

    final push

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value; 
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    var nt = 2020 + parseInt(t); 
+    var si = parseInt((p*t*r)/100);
     
+    document.getElementById("result").innerHTML = 
+    "If you deposit " + p + ", <br/>" +
+    "at an interest rate of " + r + "%. <br/>" +
+    "You will receive an amount of " + si + ", <br/>" +
+    "in the year " + nt;
 }
         
